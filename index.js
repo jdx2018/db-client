7f279fdf1bb19c44c3ad6ee0dcf60392b241c458
huī <laijinhui1202@gmail.com>
2020-08-21 10:42:42 +0800

    update jump func when token varify failed

--- index.js
+++ index.js
@@ -258,7 +258,13 @@ var dbClient = {
         con.emit(channelObj.request, JSON.stringify(data));
         con.on(channelObj.response, (res) => {
           res = JSON.parse(res);
-          if (res.code === -23) {
+          if (
+            res.code === -20 ||
+            res.code === -21 ||
+            res.code === -22 ||
+            res.code === -23 ||
+            res.code === -24
+          ) {
             window.location.href = "/sign-in";
           }
           if (needRelease) {
